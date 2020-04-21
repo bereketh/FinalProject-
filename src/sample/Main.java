a0121de9d260692e5313c08e0923726a00fb6fa2
dfaulkner1012 <derek_joseph06@hotmail.com>
2020-04-20 21:06:36 -0700

    added drop down menus and user login

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Title");
-        primaryStage.setScene(new Scene(root, 1000, 600));
+        primaryStage.setScene(new Scene(root, 800, 500));
         primaryStage.show();
     }
 
