43cb612395bedcd105e95e1f991a9b2dbaf63291
dfaulkner1012 <derek_joseph06@hotmail.com>
2020-04-26 15:30:07 -0700

    added more layout and created another fxml file

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Title");
-        primaryStage.setScene(new Scene(root, 800, 500));
+        primaryStage.setScene(new Scene(root, 450, 600));
         primaryStage.show();
     }
 
