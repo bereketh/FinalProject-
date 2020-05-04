ba4894bc32f29f0efaca3a0cd7e3dcbf87ec0bec
dfaulkner1012 <derek_joseph06@hotmail.com>
2020-05-03 18:09:38 -0700

    added a searchField to the center layout. Cleaned up code. Added a AlertHelper class to the controller. Delted recipeCompiler class.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,8 +1,25 @@
 package sample;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
 
 public class Controller {
+
+    @FXML
+    private TextField searchField;
+
+    @FXML
+    private void create(ActionEvent actionEvent) {
+        if (searchField.getText().isEmpty()) {
+            AlertHelper.showAlert(Alert.AlertType.ERROR, "Invalid Entry", "Please input your ingredients");
+        }
+        else {
+            AlertHelper.showAlert(Alert.AlertType.CONFIRMATION, "Match Found", "Here are your options");
+        }
+    }
+
     public void showNewNoteDialog(ActionEvent actionEvent) {
     }
 }
